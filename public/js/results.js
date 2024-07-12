9e2bb92f2504e6a8e2ee182d7ad145ac4e113e52
MariaPaz <joserosellonl@gmail.com>
2024-07-12 08:02:30 -0500

    Nuevo cambio

--- public/js/results.js
+++ public/js/results.js
@@ -2,13 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const params = new URLSearchParams(window.location.search);
     const search = params.get('search');
 
-    // Array con los nombres de los meses
+    
     const meses = [
         "enero", "febrero", "marzo", "abril", "mayo", "junio",
         "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
     ];
 
-    // Función para formatear la fecha en MM-DD-YYYY
     function formatDateMMDDYYYY(dateString) {
         const date = new Date(dateString);
         const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -17,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
         return `${month}-${day}-${year}`;
     }
 
-    // Función para formatear la fecha en DD de mes, YYYY
+    
     function formatDateLong(dateString) {
         const date = new Date(dateString);
         const day = date.getDate();
@@ -26,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
         return `${day} de ${month}, ${year}`;
     }
 
-    // Función para formatear la fecha en DD de mes, YYYY. HH:MM
+
     function formatDateWithTime(dateString) {
         const date = new Date(dateString);
         const day = date.getDate();
@@ -38,64 +37,79 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function fetchOrders() {
+        const selectedDate = document.getElementById('datepickerInput').value;
+        const searchParams = new URLSearchParams();
         if (search) {
-            fetch(`http://localhost:3000/api/search?keyword=${encodeURIComponent(search)}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    const resultsSection = document.querySelector('.results-section');
-                    resultsSection.innerHTML = ''; // Limpiar los resultados anteriores
-                    if (data && data.length > 0) {
-                        data.forEach((order, index) => {
-                            const resultDiv = document.createElement('div');
-                            resultDiv.classList.add('result-item');
-                            if (index === 1) resultDiv.classList.add('orange');
-                            if (index === 2) resultDiv.classList.add('green');
-                            resultDiv.innerHTML = `
-                                <div style="display: flex; justify-content: space-between;">
-                                    <div>
-                                        <p class="no-pedido"><strong>No de pedido:</strong> ${order.id}</p>
-                                    </div>
-                                    <div>
-                                        <p class="fecha-compra"><strong>Fecha de la compra:</strong> ${formatDateMMDDYYYY(order.date)}</p>
-                                    </div>
-                                </div>
-                                <hr class="linea">
-                                <div style="display: flex; justify-content: space-between;">
-                                    <div style="text-align: center;">
-                                        <img src="../public/img/ESTADO1.png" alt="Logo" style="width: 25px; height: 25px;">
-                                        <p class="pedido-realizado"><strong>Pedido Realizado:</strong></p>
-                                        <p class="fecha-realizado">${formatDateLong(order.statusDate)}</p>
-                                    </div>
-                                    <div>
-                                        ${index >= 1 ? `<div><p><strong>Estamos preparando tu pedido:</strong></p><p>${formatDateWithTime(order.preparingDate)}</p></div>` : ''}
-                                        ${index >= 2 ? `<div><p><strong>Tu pedido fue despachado:</strong></p><p>${formatDateWithTime(order.shippedDate)}</p></div>` : ''}
-                                    </div>
-                                    <div class="total-compra" style="margin-left: auto;">
-                                        <p><strong>Total de la compra:</strong> $<span class="total-compra-valor">${order.total.toFixed(2)}</span></p>
-                                    </div>
-                                </div>
-                                
-                            `;
-                            resultsSection.appendChild(resultDiv);
-                        });
-                    } else {
-                        resultsSection.textContent = 'No se encontraron resultados.';
-                    }
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                    alert('Hubo un problema al buscar el pedido. Por favor, intenta de nuevo más tarde.');
-                });
-        } else {
-            console.error('No se proporcionó ningún parámetro de búsqueda.');
+            searchParams.append('search', search);
         }
+        if (selectedDate) {
+            searchParams.append('date', selectedDate);
+        }
+
+        fetch(`http://localhost:3000/api/search?keyword=${encodeURIComponent(search)}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const resultsSection = document.querySelector('.results-section');
+                resultsSection.innerHTML = ''; 
+                if (data && data.length > 0) {
+                    data.forEach((order, index) => {
+                        const resultDiv = document.createElement('div');
+                        resultDiv.classList.add('result-item');
+                        if (index === 1) resultDiv.classList.add('orange');
+                        if (index === 2) resultDiv.classList.add('green');
+                        resultDiv.innerHTML = `
+                            <div style="display: flex; justify-content: space-between;">
+                                <div>
+                                    <p class="no-pedido"><strong>No de pedido:</strong> ${order.id}</p>
+                                </div>
+                                <div>
+                                    <p class="fecha-compra"><strong>Fecha de la compra:</strong> ${formatDateMMDDYYYY(order.date)}</p>
+                                </div>
+                            </div>
+                            <hr class="linea">
+                            <div style="display: flex; justify-content: space-between;">
+                                <div style="text-align: center;">
+                                    <img src="../public/img/ESTADO1.png" alt="Logo" style="width: 25px; height: 25px;">
+                                    <p class="pedido-realizado"><strong>Pedido Realizado:</strong></p>
+                                    <p class="fecha-realizado">${formatDateLong(order.statusDate)}</p>
+                                </div>
+                                <div>
+                                    ${index >= 1 ? `<div><p><strong>Estamos preparando tu pedido:</strong></p><p>${formatDateWithTime(order.preparingDate)}</p></div>` : ''}
+                                    ${index >= 2 ? `<div><p><strong>Tu pedido fue despachado:</strong></p><p>${formatDateWithTime(order.shippedDate)}</p></div>` : ''}
+                                </div>
+                                <div class="total-compra" style="margin-left: auto;">
+                                    <p><strong>Total de la compra:</strong> $<span class="total-compra-valor">${order.total.toFixed(2)}</span></p>
+                                </div>
+                            </div>
+                        `;
+                        resultsSection.appendChild(resultDiv);
+                    });
+                } else {
+                    resultsSection.textContent = 'No se encontraron resultados.';
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('Hubo un problema al buscar el pedido. Por favor, intenta de nuevo más tarde.');
+            });
     }
+
     fetchOrders();
 
     setInterval(fetchOrders, 5000);
+
+    
+    flatpickr('#datepickerInput', {
+        dateFormat: 'd-m-Y', 
+        
+        onChange: function(selectedDates, dateStr, instance) {
+            fetchOrders(); 
+        }
+    });
 });
+
